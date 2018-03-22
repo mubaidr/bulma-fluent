b6dd26db04298b8a0c7035f1f9d8358d6b8bee49
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-03-22 15:49:00 +0500

    add demo data

--- docs-src/src/views/demo/Elements.vue
+++ docs-src/src/views/demo/Elements.vue
@@ -1,6 +1,484 @@
 <template>
-  <div>
-    <h1>Elements</h1>
+  <div class="container">
+    <section id="box"
+             class="section">
+      <h1 class="title">Elements</h1>
+      <hr>
+
+      <h2 class="subtitle is-4">Box:</h2>
+      <div class="box">
+        <article class="media">
+          <div class="media-left">
+            <figure class="image is-64x64">
+              <img src="https://s3.amazonaws.com/uifaces/faces/twitter/zeldman/128.jpg"
+                   alt="Image">
+            </figure>
+          </div>
+          <div class="media-content">
+            <div class="content">
+              <p>
+                <strong> John Smith
+                </strong>
+                <small> @johnsmith
+                </small>
+                <small> 31m
+                </small>
+                <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+                efficitur sit amet massa fringilla egestas. Nullam condimentum luctus
+                turpis.
+              </p>
+            </div>
+            <nav class="level">
+              <div class="level-left">
+                <a class="level-item">
+                  <span class="icon is-small">
+                    <i class="fa fa-reply" />
+                  </span>
+                </a>
+                <a class="level-item">
+                  <span class="icon is-small">
+                    <i class="fa fa-retweet" />
+                  </span>
+                </a>
+                <a class="level-item">
+                  <span class="icon is-small">
+                    <i class="fa fa-heart" />
+                  </span>
+                </a>
+              </div>
+            </nav>
+          </div>
+        </article>
+      </div>
+
+      <br>
+
+      <h2 class="subtitle is-4">Buttons:</h2>
+      <div>
+        <h2 class="subtitle">Normal
+        </h2>
+        <div class="block">
+          <a class="button"> Default
+          </a>
+          <a class="button is-primary"> Primary
+          </a>
+          <a class="button is-info"> Info
+          </a>
+          <a class="button is-success"> Success
+          </a>
+          <a class="button is-warning"> Warning
+          </a>
+          <a class="button is-danger"> Danger
+          </a>
+        </div>
+        <h2 class="subtitle">Revealed (is-focused)
+        </h2>
+        <div class="block">
+          <a class="button is-focused"> Default
+          </a>
+          <a class="button is-primary is-focused"> Primary
+          </a>
+          <a class="button is-info is-focused"> Info
+          </a>
+          <a class="button is-success is-focused"> Success
+          </a>
+          <a class="button is-warning is-focused"> Warning
+          </a>
+          <a class="button is-danger is-focused"> Danger
+          </a>
+        </div>
+
+        <h2 class="subtitle">Loading
+        </h2>
+        <div class="block">
+          <a class="button is-loading"> Loading
+          </a>
+          <a class="button is-primary is-loading"> Loading
+          </a>
+          <a class="button is-info is-loading"> Loading
+          </a>
+          <a class="button is-success is-loading"> Loading
+          </a>
+          <a class="button is-warning is-loading"> Loading
+          </a>
+          <a class="button is-danger is-loading"> Loading
+          </a>
+        </div>
+
+        <h2 class="subtitle">Icons
+        </h2>
+        <p class="field">
+          <a class="button">
+            <span class="icon is-small">
+              <i class="fa fa-bold" />
+            </span>
+          </a>
+          <a class="button">
+            <span class="icon is-small">
+              <i class="fa fa-italic" />
+            </span>
+          </a>
+          <a class="button">
+            <span class="icon is-small">
+              <i class="fa fa-underline" />
+            </span>
+          </a>
+          <a class="button">
+            <span class="icon">
+              <i class="fa fa-github" />
+            </span>
+            <span> GitHub
+            </span>
+          </a>
+          <a class="button is-primary">
+            <span class="icon">
+              <i class="fa fa-twitter" />
+            </span>
+            <span> Twitter
+            </span>
+          </a>
+          <a class="button is-success">
+            <span class="icon is-small">
+              <i class="fa fa-check" />
+            </span>
+            <span> Save
+            </span>
+          </a>
+          <a class="button is-danger">
+            <span> Delete
+            </span>
+            <span class="icon is-small">
+              <i class="fa fa-times" />
+            </span>
+          </a>
+        </p>
+
+        <h2 class="subtitle">Outlined
+        </h2>
+        <div class="block">
+          <a class="button is-outlined"> Outlined
+          </a>
+          <a class="button is-primary is-outlined"> Outlined
+          </a>
+          <a class="button is-info is-outlined"> Outlined
+          </a>
+          <a class="button is-success is-outlined"> Outlined
+          </a>
+          <a class="button is-warning is-outlined"> Outlined
+          </a>
+          <a class="button is-danger is-outlined"> Outlined
+          </a>
+        </div>
+
+        <h2 class="subtitle">Grouped
+        </h2>
+        <div class="field is-grouped">
+          <p class="buttons has-addons">
+            <a class="button">
+              <span class="icon is-small">
+                <i class="fa fa-bold" />
+              </span>
+              <span> Bold
+              </span>
+            </a>
+            <a class="button">
+              <span class="icon is-small">
+                <i class="fa fa-italic" />
+              </span>
+              <span> Italic
+              </span>
+            </a>
+            <a class="button">
+              <span class="icon is-small">
+                <i class="fa fa-underline" />
+              </span>
+              <span> Underline
+              </span>
+            </a>
+            <a class="button">
+              <span class="icon is-small">
+                <i class="fa fa-align-left" />
+              </span>
+              <span> Left
+              </span>
+            </a>
+            <a class="button">
+              <span class="icon is-small">
+                <i class="fa fa-align-center" />
+              </span>
+              <span> Center
+              </span>
+            </a>
+            <a class="button">
+              <span class="icon is-small">
+                <i class="fa fa-align-right" />
+              </span>
+              <span> Right
+              </span>
+            </a>
+          </p>
+        </div>
+        <br>
+        <h2 class="subtitle">Sizes
+        </h2>
+        <div class="block">
+          <a class="button is-small"> Small
+          </a>
+          <a class="button"> Normal
+          </a>
+          <a class="button is-medium"> Medium
+          </a>
+          <a class="button is-large"> Large
+          </a>
+        </div>
+      </div>
+
+      <br>
+
+      <h2 class="subtitle is-4">Progress bars:</h2>
+      <div id="progress">
+        <h2 class="subtitle"> Indeterminate
+        </h2>
+        <progress class="progress"
+                  max="1000" />
+        <h2 class="subtitle"> Determinate
+        </h2>
+        <progress class="progress"
+                  max="100"
+                  value="15"> 15%
+        </progress>
+        <progress class="progress is-primary"
+                  max="100"
+                  value="30"> 30%
+        </progress>
+        <progress class="progress is-info"
+                  max="100"
+                  value="45"> 45%
+        </progress>
+        <progress class="progress is-success"
+                  max="100"
+                  value="60"> 60%
+        </progress>
+        <progress class="progress is-warning"
+                  max="100"
+                  value="75"> 75%
+        </progress>
+        <progress class="progress is-danger"
+                  max="100"
+                  value="90"> 90%
+        </progress>
+      </div>
+
+      <br>
+
+      <h2 class="subtitle is-4">Notifications:</h2>
+      <div class="columns">
+        <div class="column">
+          <div class="notification">
+            <button class="delete" /> Lorem ipsum dolor sit amet,
+            <a href="#">consectetur
+            </a> adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing
+            elit
+          </div>
+          <div class="notification is-primary">
+            <button class="delete" /> Primar lorem ipsum dolor sit amet,
+            <a href="#">consectetur
+            </a> adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing
+            elit
+          </div>
+          <div class="notification is-info">
+            <button class="delete" /> Info lorem ipsum dolor sit amet,
+            <a href="#">consectetur
+            </a> adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing
+            elit
+          </div>
+        </div>
+        <div class="column">
+          <div class="notification is-success">
+            <button class="delete" /> Success lorem ipsum dolor sit amet,
+            <a href="#">consectetur
+            </a> adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing
+            elit
+          </div>
+          <div class="notification is-warning">
+            <button class="delete" /> Warning lorem ipsum dolor sit amet,
+            <a href="#">consectetur
+            </a> adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing
+            elit
+          </div>
+          <div class="notification is-danger">
+            <button class="delete" /> Danger lorem ipsum dolor sit amet,
+            <a href="#">consectetur
+            </a> adipiscing elit lorem ipsum dolor sit amet, consectetur adipiscing
+            elit
+          </div>
+        </div>
+      </div>
+
+      <br>
+
+      <h2 class="subtitle is-4">Tag:</h2>
+      <div class="tags">
+        <span class="tag is-white"> White
+        </span>
+        <span class="tag is-light"> Light
+        </span>
+        <span class="tag is-dark"> Dark
+        </span>
+        <span class="tag is-black"> Black
+        </span>
+        <span class="tag is-primary"> Primary
+        </span>
+        <span class="tag is-info"> Info
+        </span>
+        <span class="tag is-success"> Success
+        </span>
+        <span class="tag is-warning"> Warning
+        </span>
+        <span class="tag is-danger"> Danger
+        </span>
+        <span class="tag is-primary is-medium"> Medium
+        </span>
+        <span class="tag is-info is-large"> Large
+        </span>
+        <span class="tag is-success"> Bar
+          <button class="delete is-small" />
+        </span>
+        <span class="tag is-warning is-medium"> Hello
+          <button class="delete is-small" />
+        </span>
+        <span class="tag is-danger is-large"> World
+          <button class="delete" />
+        </span>
+      </div>
+
+      <br>
+
+      <h2 class="subtitle is-4">Tables:</h2>
+      <div class="columns">
+        <div class="column">
+          <h2 class="subtitle">Default</h2>
+          <table class="table is-fullwidth is-hoverable">
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr class="is-selected">
+              <td>Centro comercial</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+            <tr>
+              <td>Ernst Handel</td>
+              <td>Roland Mendel</td>
+              <td>Austria</td>
+            </tr>
+            <tr>
+              <td>Island Trading</td>
+              <td>Helen Bennett</td>
+              <td>UK</td>
+            </tr>
+            <tr>
+              <td>Laughing Bacchus Winecellars</td>
+              <td>Yoshi Tannamuri</td>
+              <td>Canada</td>
+            </tr>
+            <tr>
+              <td>Magazzini Alimentari Riuniti</td>
+              <td>Giovanni Rovelli</td>
+              <td>Italy</td>
+            </tr>
+          </table>
+        </div>
+        <div class="column">
+
+          <h2 class="subtitle">Striped</h2>
+          <table class="table is-fullwidth is-striped">
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr>
+              <td>Centro comercial</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+            <tr>
+              <td>Ernst Handel</td>
+              <td>Roland Mendel</td>
+              <td>Austria</td>
+            </tr>
+            <tr>
+              <td>Island Trading</td>
+              <td>Helen Bennett</td>
+              <td>UK</td>
+            </tr>
+            <tr>
+              <td>Laughing Bacchus Winecellars</td>
+              <td>Yoshi Tannamuri</td>
+              <td>Canada</td>
+            </tr>
+            <tr>
+              <td>Magazzini Alimentari Riuniti</td>
+              <td>Giovanni Rovelli</td>
+              <td>Italy</td>
+            </tr>
+          </table>
+        </div>
+        <div class="column">
+          <h2 class="subtitle">Bordered</h2>
+          <table class="table is-fullwidth is-bordered is-hoverable">
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr>
+              <td>Centro comercial</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+            <tr>
+              <td>Ernst Handel</td>
+              <td>Roland Mendel</td>
+              <td>Austria</td>
+            </tr>
+            <tr>
+              <td>Island Trading</td>
+              <td>Helen Bennett</td>
+              <td>UK</td>
+            </tr>
+            <tr>
+              <td>Laughing Bacchus Winecellars</td>
+              <td>Yoshi Tannamuri</td>
+              <td>Canada</td>
+            </tr>
+            <tr>
+              <td>Magazzini Alimentari Riuniti</td>
+              <td>Giovanni Rovelli</td>
+              <td>Italy</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
--- docs-src/src/views/demo/Form.vue
+++ docs-src/src/views/demo/Form.vue
@@ -1,6 +1,251 @@
 <template>
-  <div>
-    <h1>Form</h1>
+  <div class="container">
+    <section id="form"
+             class="section">
+      <h1 class="title"> Form
+      </h1>
+      <hr>
+      <div class="columns">
+        <div class="column">
+          <h4 class="subtitle"> Horizontal
+          </h4>
+          <div class="field">
+            <label class="label">Name
+            </label>
+            <p class="control">
+              <input class="input"
+                     type="text"
+                     placeholder="Text input">
+            </p>
+          </div>
+          <div class="field">
+            <label class="label">Username
+            </label>
+            <p class="control">
+              <input class="input"
+                     type="text"
+                     placeholder="Text input">
+            </p>
+          </div>
+          <div class="field">
+            <label class="label">Email
+            </label>
+            <p class="control">
+              <input class="input"
+                     type="text"
+                     placeholder="Email input">
+            </p>
+          </div>
+          <div class="field">
+            <label class="label">Subject
+            </label>
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option>Select dropdown
+                  </option>
+                  <option>With options
+                  </option>
+                </select>
+              </span>
+            </p>
+          </div>
+          <div class="field">
+            <label class="label">Message
+            </label>
+            <p class="control">
+              <textarea class="textarea"
+                        placeholder="Textarea" />
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <label class="checkbox">
+                <input type="checkbox"> I agree to the
+                <a href="#">terms and conditions
+                </a>
+              </label>
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <label class="radio">
+                <input name="question"
+                       type="radio"> Yes
+              </label>
+              <label class="radio">
+                <input name="question"
+                       type="radio"> No
+              </label>
+            </p>
+          </div>
+
+          <div class="field has-addons">
+            <p class="control">
+              <span class="select">
+                <select>
+                  <option>$
+                  </option>
+                  <option>£
+                  </option>
+                  <option>€
+                  </option>
+                </select>
+              </span>
+            </p>
+            <p class="control is-expanded">
+              <input class="input"
+                     type="text"
+                     placeholder="Amount of money">
+            </p>
+            <p class="control">
+              <a class="button">
+                Transfer
+              </a>
+            </p>
+          </div>
+          <div class="field has-addons">
+            <p class="control is-expanded">
+              <span class="select is-fullwidth">
+                <select name="country">
+                  <option value="Argentina">Argentina
+                  </option>
+                  <option value="Bolivia">Bolivia
+                  </option>
+                  <option value="Brazil">Brazil
+                  </option>
+                  <option value="Chile">Chile
+                  </option>
+                  <option value="Colombia">Colombia
+                  </option>
+                  <option value="Ecuador">Ecuador
+                  </option>
+                  <option value="Guyana">Guyana
+                  </option>
+                  <option value="Paraguay">Paraguay
+                  </option>
+                  <option value="Peru">Peru
+                  </option>
+                  <option value="Suriname">Suriname
+                  </option>
+                  <option value="Uruguay">Uruguay
+                  </option>
+                  <option value="Venezuela">Venezuela
+                  </option>
+                </select>
+              </span>
+            </p>
+            <p class="control">
+              <button class="button"
+                      type="submit">Choose
+              </button>
+            </p>
+          </div>
+
+          <div class="field">
+            <p class="control has-icons-left">
+              <input class="input"
+                     type="email"
+                     placeholder="Email">
+              <span class="icon is-small is-left">
+                <i class="fa fa-envelope" />
+              </span>
+            </p>
+          </div>
+          <div class="field">
+            <p class="control has-icons-left">
+              <input class="input"
+                     type="password"
+                     placeholder="Password">
+              <span class="icon is-small is-left">
+                <i class="fa fa-lock" />
+              </span>
+            </p>
+          </div>
+
+          <div class="field is-grouped">
+            <p class="control">
+              <button class="button is-primary">Submit
+              </button>
+            </p>
+            <p class="control">
+              <button class="button">Cancel
+              </button>
+            </p>
+          </div>
+        </div>
+        <div class="column">
+          <h3 class="subtitle"> Sizes
+          </h3>
+          <div class="field">
+            <p class="control">
+              <input class="input is-small"
+                     type="text"
+                     placeholder="Small input">
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <input class="input"
+                     type="text"
+                     placeholder="Normal input">
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <input class="input is-medium"
+                     type="text"
+                     placeholder="Medium input">
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <input class="input is-large"
+                     type="text"
+                     placeholder="Large input">
+            </p>
+          </div>
+          <br>
+          <h3 class="subtitle"> Colors
+          </h3>
+          <div class="field">
+            <p class="control">
+              <input class="input is-primary"
+                     type="text"
+                     placeholder="Primary input">
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <input class="input is-info"
+                     type="text"
+                     placeholder="Info input">
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <input class="input is-success"
+                     type="text"
+                     placeholder="Success input">
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <input class="input is-warning"
+                     type="text"
+                     placeholder="Warning input">
+            </p>
+          </div>
+          <div class="field">
+            <p class="control">
+              <input class="input is-danger"
+                     type="text"
+                     placeholder="Danger input">
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
