d10f337f922b1129a205f3f94332754b74d4f4ab
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-03-26 10:58:09 +0500

    update typography, simplify color pallete

--- docs-src/src/views/Header.vue
+++ docs-src/src/views/Header.vue
@@ -19,7 +19,7 @@
           </h2>
           <br>
           <p>
-            <a class="button"
+            <a class="button is-focused"
                href="https://github.com/mubaidr/bulma-fluent"
                title="Github Repository"
                target="_blank">
