bebefcd7bcb4664e15640ffe51b74373cc666a05
Muhammad Ubaid Raza <mubaidr@gmail.com>
2018-06-01 12:39:24 +0500

    update docs

--- src/docs/src/views/demo/Extras.vue
+++ src/docs/src/views/demo/Extras.vue
@@ -11,6 +11,24 @@
               <div class="content">
                 <p>
                   <strong>
+                    <a href="//github.com/mubaidr/vue-fluent">Vue-Fluent</a>
+                  </strong>
+                  <small>by
+                    <a href="//github.com/mubaidr">mubaidr</a>
+                  </small>
+                  <br> Fluent Design Components for Vue.js using Bulma, inspired by Microsoft’s Fluent Design System:
+                  <a href="//mubaidr.github.io/bulma-addons">Homepage</a>
+                </p>
+              </div>
+            </div>
+          </article>
+        </div>
+        <div class="box">
+          <article class="media">
+            <div class="media-content">
+              <div class="content">
+                <p>
+                  <strong>
                     <a href="//github.com/mubaidr/bulma-addons">Bulma-Addons</a>
                   </strong>
                   <small>by
